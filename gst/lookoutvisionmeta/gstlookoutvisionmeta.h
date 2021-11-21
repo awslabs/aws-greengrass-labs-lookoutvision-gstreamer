3332730e0868de5112dd1f766c210f9e023ca946
Anmol Popli <popanmol@amazon.com>
2021-11-21 15:11:59 -0800

    Initial commit

---
+++ gst/lookoutvisionmeta/gstlookoutvisionmeta.h
@@ -0,0 +1,34 @@
+#ifndef __GST_LOOKOUTVISION_META_H__
+#define __GST_LOOKOUTVISION_META_H__
+
+#include <gst/gst.h>
+#include "gstlookoutvisionresult.h"
+
+G_BEGIN_DECLS
+
+typedef struct _GstLookoutVisionMeta {
+    GstMeta meta;
+
+    GstLookoutVisionResult* result;
+} GstLookoutVisionMeta;
+
+#define GST_LOOKOUT_VISION_META_NAME "GstLookoutVisionMeta"
+#define GST_LOOKOUT_VISION_META_API_NAME "GstLookoutVisionMetaAPI"
+
+#define GST_LOOKOUT_VISION_META_API_TYPE (gst_lookout_vision_meta_api_get_type())
+#define GST_LOOKOUT_VISION_META_INFO (gst_lookout_vision_meta_get_info())
+
+GST_EXPORT
+        GType gst_lookout_vision_meta_api_get_type (void);
+GST_EXPORT
+        const GstMetaInfo* gst_lookout_vision_meta_get_info (void);
+
+GST_EXPORT
+        GstLookoutVisionMeta* gst_buffer_add_lookout_vision_meta (GstBuffer *buffer, GstLookoutVisionResult* result);
+
+GST_EXPORT
+        GstLookoutVisionMeta* gst_buffer_get_lookout_vision_meta (GstBuffer *buffer);
+
+G_END_DECLS
+
+#endif /* __GST_LOOKOUTVISION_META_H__ */
---
+++ lookoutvision-client/LookoutVisionInferenceClient.h
@@ -0,0 +1,42 @@
+#ifndef __LOOKOUTVISION_INFERENCE_CLIENT_H__
+#define __LOOKOUTVISION_INFERENCE_CLIENT_H__
+
+#include <glib.h>
+#include "Inference.grpc.pb.h"
+#include "gst/lookoutvisionmeta/gstlookoutvisionresult.h"
+
+class LookoutVisionInferenceClient{
+public:
+    typedef enum _OperationStatus {
+        SUCCESSFUL = 0,
+        FAILED = -1
+    } OperationStatus;
+
+    LookoutVisionInferenceClient(std::string server_url);
+    ~LookoutVisionInferenceClient();
+    void setServerUrl(std::string server_url);
+    GstLookoutVisionResult* DetectAnomalies(std::string model_component, guint8* frame, size_t bytes_size, size_t width,
+                                            size_t height);
+    OperationStatus StartModel(std::string model_component, int model_status_timeout);
+
+private:
+    static const int POLLING_INTERVAL_IN_SECONDS;
+    #ifdef SHARED_MEMORY
+    static const std::string SHM_NAME;
+    size_t shm_size = 1920*1920*3;
+    int shm_fd;
+    uint8_t* shm_data;
+    #endif
+    std::string server_url;
+    std::shared_ptr<grpc::Channel> channel;
+    std::unique_ptr<AWS::LookoutVision::EdgeAgent::Stub> stub;
+
+    #ifdef SHARED_MEMORY
+    void writeSHM(guint8* buf, size_t bytes_size);
+    #endif
+    bool waitForModelStatusWithTimeout(std::string model_component,
+                                       AWS::LookoutVision::ModelStatus expected_status, int timeout_in_seconds);
+    AWS::LookoutVision::ModelStatus* getModelStatus(std::string model_component);
+};
+
+#endif
